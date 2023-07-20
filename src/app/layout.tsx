5d31841dd6c675e35109b88fa38bdea25a517065
Kota Lovan <avery.malaythong@techjourney.org>
2023-07-20 20:36:34 +0000

    removing pokemon and rick and morty

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,6 @@ TODO:  Change these things along with:
 const font = Rajdhani({ weight: '400', subsets: ['latin']})
 const title = '...';
 const description = 'This the Tech Journey template site';
-const links = [
-  {title: 'no', href: '/pokemon'},
-  
-];
 const SocialLinks = {
   twitter: '#twitter',
   github: '#github',
@@ -39,7 +35,7 @@ export default function RootLayout({
   return (
       <html lang="en" className={'h-full'}>
       <body className={`${font.className} flex flex-col min-h-screen`}>
-      <NavBarContainer title={title} links={links}>
+      <NavBarContainer title={title}>
         <main className={'flex-1 max-w-6xl py-8 md:py-16 px-4 md:px-0'}>{children}</main>
       </NavBarContainer>
       <Footer socialLinks={SocialLinks}/>
